14a843d30465742ea396b428ce3d8504884a1d70
Catiflam <gilfrid1@gmail.com>
2023-08-24 16:50:58 +0200

    edit team log

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const team = [
+	{
+		name: "Wayne Barnett",
+		profession: "Founder & CEO",
+		info: ["wayne-barnett-founder-ceo.jpg"],
+	},
+	{
+		name: "Angela Caroll",
+		profession: "Chief Editor",
+		info: ["angela-caroll-chief-editor.jpg"],
+	},
+	{
+		name: "Walter Gordon",
+		profession: "Office Manager",
+		info: ["walter-gordon-office-manager.jpg"],
+	},
+	{
+		name: "Angela Lopez",
+		profession: "Social Media Manager",
+		info: ["angela-lopez-social-media-manager.jpg"],
+	},
+	{
+		name: "Scott Estrada",
+		profession: "Developer",
+		info: ["scott-estrada-developer.jpg"],
+	},
+	{
+		name: "Barbara Ramos	",
+		profession: "Graphic Designer",
+		info: ["barbara-ramos-graphic-designer.jpg"],
+	},
+];
+
+console.log(team[0]);
+console.log(team[1]);
+console.log(team[2]);
+console.log(team[3]);
+console.log(team[4]);
+console.log(team[5]);
